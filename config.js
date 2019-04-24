28f492fd5502cb481a5aabb49f14755ade83ce6b
Kim Fisher <kim@squarewater.com>
2019-04-24 07:28:46 -0400

    streamlined test functions, refactoring and cleanup

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
+var Color = require('color');
 var config = {};
 
-// on a Pi this should I think be '/dev/serial0' but may need to do some research depending on model/environment
 config.BOARD_PORT = "/dev/ttyACM0";
 config.PIN_AZIMUTH = 9;
 config.PIN_ALTITUDE = 10;
@@ -12,8 +12,9 @@ config.CW_ALTITUDE = 0;
 config.CCW_AZIMUTH = 1;
 config.CCW_ALTITUDE = 1;
 
-config.INTERVAL = 5000;
-config.MOVEMENT_THRESHOLD_AZI = 0.5;  // under this # of seconds, don't move
+config.INIT_CALIBRATION_INTERVAL = 60 * 1000;
+config.INTERVAL = 10 * 1000;
+config.MOVEMENT_THRESHOLD_AZI = 0.1;  // under this # of seconds, don't move
 config.MOVEMENT_THRESHOLD_ALT = 0.001;
 
 // multipliers to convert degrees to seconds of continuous servo motion
@@ -22,23 +23,29 @@ config.MOVEMENT_THRESHOLD_ALT = 0.001;
 config.DEGREES2SECONDS_AZIMUTH_CW = 0.262;
 config.DEGREES2SECONDS_AZIMUTH_CCW = 0.2679;
 config.SPEED_AZIMUTH = 0.5;
-config.DEGREES2SECONDS_ALTITUDE_CW = 0.0065;  // 0.0065;
-config.DEGREES2SECONDS_ALTITUDE_CCW = 0.0055;  // 0.0055;
+config.DEGREES2SECONDS_ALTITUDE_CW = 0.0065;
+config.DEGREES2SECONDS_ALTITUDE_CCW = 0.0065;  // 0.0055;
 config.SPEED_ALTITUDE_CW = 0.19;
-config.SPEED_ALTITUDE_CCW = 0.15;
+config.SPEED_ALTITUDE_CCW = 0.19;
+//~ config.SPEED_ALTITUDE_CW = 0.19;
+//~ config.SPEED_ALTITUDE_CCW = 0.15;
 
-config.LAT = 40.760248;
-config.LON = -73.929272;
+config.LAT = 40.769200;  // 40.760248; (B6)
+config.LON = -73.936672;  // -73.929272; (B6)
+
+config.LIGHT_STRIPS = [ {pin: 6, length: 60}, ];
+config.LIGHT_INTERVAL = 60 * 1000;
+config.LIGHT_COLOR = Color.rgb(128, 128, 128);  // 1/2 max
+//~ config.LIGHT_COLOR = Color.rgb(255, 255, 255);  // full white
+//~ config.LIGHT_COLOR = Color.rgb(64, 64, 64);  // 1/4 max
+// dimmest: Color.rgb(28, 28, 28)
+//~ config.LIGHT_COLOR = Color('#333');
+//~ config.LIGHT_COLOR.lighten(0.2);
 
 module.exports = config;
 
 
-
-//~ var five = require("johnny-five");  // http://johnny-five.io/
-//~ var config = {};
-
-//~ // on a Pi this should I think be '/dev/serial0' but may need to do some research depending on model/environment
-//~ config.BOARD_PORT = "/dev/ttyACM0";
+// abortive stepper settings
 //~ config.STEPSPERREV = 200;  // Typical stepper motors are 1.8 degrees per step, which is 200 steps per revolution
 //~ config.PINS_AZIMUTH = {
   //~ step: 2,
@@ -57,10 +64,3 @@ module.exports = config;
 //~ config.RPM_ALTITUDE = 180;
 //~ config.DEGREES2STEPS_AZIMUTH = 2100;  // would be 0.5555 without gearing
 //~ config.DEGREES2STEPS_ALTITUDE = 360;
-
-//~ config.INTERVAL = 5000;
-
-//~ config.LAT = 40.760248;
-//~ config.LON = -73.929272;
-
-//~ module.exports = config;
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var events = require('events');
 var suncalc = require('suncalc');  // https://github.com/mourner/suncalc
 var five = require("johnny-five");  // http://johnny-five.io/
+var pixel = require("node-pixel");  // https://github.com/ajfisher/node-pixel
 const config = require('./config');
 
 console.info("Initializing port...");
@@ -18,24 +19,13 @@ var Arrow = function() {
     pin: config.PIN_AZIMUTH,
     deadband: config.DEADBAND_AZIMUTH
   });
-  //~ this.servo_altitude = new five.Servo({
-    //~ pin: config.PIN_ALTITUDE,
-    //~ deadband: config.DEADBAND_ALTITUDE
-    //~ center: true,
-    //~ range: [-90, 90],
-    //~ startAt: 0,
-    //~ deviceRange: [-360, 360],
-  //~ });
   this.servo_altitude = new five.Servo.Continuous({
     pin: config.PIN_ALTITUDE,
     deadband: config.DEADBAND_ALTITUDE
   });
   this.imu = new five.IMU({
     controller: "BNO055",
-    // enableExternalCrystal: false
-  });
-  this.compass = new five.Compass({
-    controller: "BNO055"
+    enableExternalCrystal: true
   });
 
   this.azimuth = 0;
@@ -44,26 +34,29 @@ var Arrow = function() {
   this.moving_altitude = false;
   //~ console.log("azi startAt: " + this.servo_azimuth.startAt);
   //~ console.log("alt startAt: " + this.servo_altitude.startAt);
-};
-Arrow.prototype = new events.EventEmitter;
 
+  this.light = new pixel.Strip({
+      board: board,
+      controller: "FIRMATA",
+      strips: config.LIGHT_STRIPS,
+      gamma: 2.8, // set to a gamma that works nicely for WS2812
+  });
+};
+
+Arrow.prototype = new events.EventEmitter;
 Arrow.prototype.on('done:azimuth', function() {
-  var self = this;
-  self.moving_azimuth = false;
-  //~ self.azimuth = Math.floor(self.imu.magnetometer.heading);
-  //~ console.log("end azimuth: " + self.azimuth);
-  //~ console.log("end altitude: " + self.altitude);
-  //~ console.log("azi servo position: " + self.servo_azimuth.position);
+  this.moving_azimuth = false;
+  //~ console.log("end azimuth: " + this.azimuth);
+  //~ console.log("end altitude: " + this.altitude);
+  //~ console.log("azi servo position: " + this.servo_azimuth.position);
 });
 Arrow.prototype.on('done:altitude', function() {
-  var self = this;
-  self.moving_altitude = false;
-  //~ self.altitude = Math.floor(self.imu.accelerometer.pitch);
-  //~ console.log("end azimuth: " + self.azimuth);
-  //~ console.log("end altitude: " + self.altitude);
-  //~ console.log("alt servo position: " + self.servo_altitude.position);
+  this.moving_altitude = false;
+  //~ console.log("end azimuth: " + this.azimuth);
+  //~ console.log("end altitude: " + this.altitude);
+  //~ console.log("alt servo position: " + this.servo_altitude.position);
 });
-
+  
 
 Arrow.prototype.moveDegrees = function(
   type,
@@ -75,21 +68,22 @@ Arrow.prototype.moveDegrees = function(
   var servo = (type === 'azimuth') ? self.servo_azimuth : self.servo_altitude;
   var multiplier = config.DEGREES2SECONDS_AZIMUTH_CW;
   var threshold = config.MOVEMENT_THRESHOLD_AZI;
+  var dirlabel = '';
   if (type === 'altitude') {
     multiplier = (direction === config.CW_ALTITUDE) ? config.DEGREES2SECONDS_ALTITUDE_CW : config.DEGREES2SECONDS_ALTITUDE_CCW;
+    dirlabel = (direction === config.CW_ALTITUDE) ? 'clockwise' : 'counter-clockwise' ;
     threshold = config.MOVEMENT_THRESHOLD_ALT;
   } else if (type === 'azimuth') {
     multiplier = (direction === config.CW_AZIMUTH) ? config.DEGREES2SECONDS_AZIMUTH_CW : config.DEGREES2SECONDS_AZIMUTH_CCW;
+    dirlabel = (direction === config.CW_AZIMUTH) ? 'clockwise' : 'counter-clockwise' ;
     threshold = config.MOVEMENT_THRESHOLD_AZI;
   }
   //~ console.log('multiplier: ' + multiplier);
 
   var seconds = degrees * multiplier * (1 / speed);
   // above theoretically correct but not working for variable speed with these servos
-  //~ var seconds = degrees * multiplier * (1 / speed) + (1 / speed);
   if (seconds < threshold) {
-    console.log('Not moving ' + type + ' because required ' + seconds + ' seconds is less than ' +
-      threshold);
+    console.log('Not moving ' + type + ' because required ' + seconds + ' seconds is less than ' + threshold);
     if (type === 'altitude') {
       self.moving_altitude = false;
     } else {
@@ -98,11 +92,10 @@ Arrow.prototype.moveDegrees = function(
     return;
   }
 
+  console.log('moving ' + type + ' ' + dirlabel + ' ' + seconds + ' seconds speed ' + speed);
   if (direction === 1) {
-    console.log('moving ' + type + ' clockwise ' + seconds + ' seconds speed ' + speed);
     servo.cw(speed);
-  } else {
-    console.log('moving ' + type + ' counter-clockwise ' + seconds + ' seconds speed ' + speed);
+  } else if (direction === 0) {
     servo.ccw(speed);
   }
 
@@ -133,9 +126,11 @@ Arrow.prototype.moveToAzimuth = function(
     direction = Math.abs(direction - 1);
   }
 
-  console.log('azi degreesToMove: ' + degreesToMove + ' direction: ' + direction);
-  self.moving_azimuth = true;
-  self.moveDegrees('azimuth', degreesToMove, direction, config.SPEED_AZIMUTH);
+  if (degreesToMove > 0) {
+    console.log('azi degreesToMove: ' + degreesToMove + ' direction: ' + direction);
+    self.moving_azimuth = true;
+    self.moveDegrees('azimuth', degreesToMove, direction, config.SPEED_AZIMUTH);
+  }
 };
 
 
@@ -153,53 +148,30 @@ Arrow.prototype.moveToAltitude = function(
   if (degrees - self.altitude < 0) direction = config.CCW_ALTITUDE;
   var speed = (direction === config.CW_ALTITUDE) ? config.SPEED_ALTITUDE_CW : config.SPEED_ALTITUDE_CCW;
 
-  console.log('alt degreesToMove: ' + degreesToMove + ' direction: ' + direction);
-  self.moving_altitude = true;
-  self.moveDegrees('altitude', degreesToMove, direction, speed);
+  if (degreesToMove > 0) {
+    console.log('alt degreesToMove: ' + degreesToMove + ' direction: ' + direction);
+    self.moving_altitude = true;
+    self.moveDegrees('altitude', degreesToMove, direction, speed);
+  }
 };
 
 
 var trackMoon = function() {
-  var arrow = new Arrow();
-  arrow.imu.on("data", function() {
-    arrow.azimuth = Math.floor(arrow.imu.orientation.euler.heading);
-    arrow.altitude = -Math.floor(arrow.imu.orientation.euler.pitch);
-    //~ console.log("azimuth: " + arrow.azimuth + " altitude: " + arrow.altitude);
-  });
-
   var arrowInterval = setInterval(function() {
     if (arrow.imu.gyro.isCalibrated) {
+      var now = new Date();
       // From https://github.com/mourner/suncalc, south = 0: 
       // "azimuth in radians (direction along the horizon, measured from south to west), 
       // e.g. 0 is south and Math.PI * 3/4 is northwest"
-      var moonPos = suncalc.getMoonPosition(new Date(), config.LAT, config.LON);
+      var moonPos = suncalc.getMoonPosition(now, config.LAT, config.LON);
       var moon_azimuth = Math.floor(180 + radians2Degrees(moonPos['azimuth']));
       if (moon_azimuth > 360) {
         moon_azimuth = Math.abs(360 - moon_azimuth);
       }
       var moon_altitude = Math.floor(radians2Degrees(moonPos['altitude']));
-      console.log('----- ' + new Date() + ' -----');
+      console.log('----- ' + now + ' -----');
       console.log('moon  azimuth: ' + moon_azimuth  + ' moon altitude:  ' + moon_altitude);
       console.log('arrow azimuth: ' + arrow.azimuth + ' arrow altitude: ' + arrow.altitude);
-      //~ console.log("Accelerometer");
-      //~ console.log("  pitch        : ", arrow.imu.accelerometer.pitch);
-      //~ console.log("  roll         : ", arrow.imu.accelerometer.roll);
-      //~ console.log("  acceleration : ", arrow.imu.accelerometer.acceleration);
-      //~ console.log("  inclination  : ", arrow.imu.accelerometer.inclination);
-      //~ console.log("  orientation  : ", arrow.imu.accelerometer.orientation);
-      //~ console.log("--------------------------------------");
-      //~ console.log("Gyroscope");
-      //~ console.log("  x            : ", arrow.imu.gyro.x);
-      //~ console.log("  y            : ", arrow.imu.gyro.y);
-      //~ console.log("  z            : ", arrow.imu.gyro.z);
-      //~ console.log("  pitch        : ", arrow.imu.gyro.pitch);
-      //~ console.log("  roll         : ", arrow.imu.gyro.roll);
-      //~ console.log("  yaw          : ", arrow.imu.gyro.yaw);
-      //~ console.log("  rate         : ", arrow.imu.gyro.rate);
-      //~ console.log("  isCalibrated : ", arrow.imu.gyro.isCalibrated);
-      //~ console.log("--------------------------------------");
-      //~ console.log("magnetometer : ", Math.floor(arrow.imu.magnetometer.heading));
-      //~ console.log("--------------------------------------");
 
       if (!arrow.moving_azimuth) {
         //~ console.log("start azimuth: " + arrow.azimuth);
@@ -219,49 +191,73 @@ var trackMoon = function() {
   }, config.INTERVAL);
 };
 
+var setLight = function() {
+  var lightInterval = setInterval(function() {
+    let now = new Date();
+    let suntimes = suncalc.getTimes(now, config.LAT, config.LON);
+    //~ console.log(suntimes);
+    let light_begins = suntimes['goldenHourEnd'].getTime();  // morning golden hour ends
+    let light_ends = suntimes['goldenHour'].getTime();  // evening golden hour starts
+    if (!(now.getTime() > light_begins && now.getTime() < light_ends)) {
+      console.log("nighttime: turning lights on");
+      arrow.light.color(config.LIGHT_COLOR.string());
+      arrow.light.show();
+    } else{
+      console.log("daytime: turning lights off");
+      arrow.light.off();
+    }
+  }, config.LIGHT_INTERVAL);
+};
 
-var testServos = function() {
-  var arrow = new Arrow();
-  var testDegrees = 2;
+var manualControl = function(testDegrees) {
+  testDegrees = testDegrees || 5;
 
   process.stdin.resume();
   process.stdin.setEncoding("utf8");
   process.stdin.setRawMode(true);
-
-  arrow.imu.on("data", function() {
-    arrow.azimuth = Math.floor(arrow.imu.magnetometer.heading);
-    arrow.altitude = Math.floor(arrow.imu.accelerometer.pitch);
-    //~ console.log("azi: " + arrow.azimuth + " compass: " + Math.floor(arrow.compass.heading) + " alt: " + arrow.altitude);
-  });
-
   process.stdin.on("keypress", function(ch, key) {
     if (!key) {
       return;
     }
-    if (arrow.imu.gyro.isCalibrated) {
-      console.log("start azimuth: " + arrow.azimuth);
-      console.log("start altitude: " + arrow.altitude);
+    if (key.name === "q") {
+      console.log("Quitting");
+      arrow.light.off();
+      setTimeout(process.exit, 1000);
+      //~ process.exit();
+    } else if (key.sequence === ",") {
+      console.log(config.LIGHT_COLOR);
+      arrow.light.color(config.LIGHT_COLOR.string());
+      arrow.light.show();
+    } else if (key.sequence === ".") {
+      console.log("turning lights off");
+      arrow.light.off();
+    } else if (key.name === "up") {
+      console.log("alt CW " + testDegrees + " degrees");
+      arrow.moveDegrees('altitude', testDegrees, config.CW_ALTITUDE, config.SPEED_ALTITUDE_CW);
+    } else if (key.name === "down") {
+      console.log("alt CCW " + testDegrees + " degrees");
+      arrow.moveDegrees('altitude', testDegrees, config.CCW_ALTITUDE, config.SPEED_ALTITUDE_CCW);
+    } else if (key.name === "left") {
+      console.log("azi CCW " + testDegrees + " degrees");
+      arrow.moveDegrees('azimuth', testDegrees, config.CCW_AZIMUTH, config.SPEED_AZIMUTH);
+    } else if (key.name === "right") {
+      console.log("azi CW " + testDegrees + " degrees");
+      arrow.moveDegrees('azimuth', testDegrees, config.CW_AZIMUTH, config.SPEED_AZIMUTH);
+    } else if (key.name === "space") {
+      console.log("hit space; stopping");
+      arrow.moveDegrees('azimuth', 0.2, config.CW_AZIMUTH, config.SPEED_AZIMUTH);
+      arrow.moveDegrees('altitude', 0.1, config.CW_AZIMUTH, config.SPEED_AZIMUTH);
+    }
+  });
+}
 
-      if (key.name === "q") {
-        console.log("Quitting");
-        process.exit();
-      } else if (key.name === "up") {
-        console.log("alt CW " + testDegrees + " degrees");
-        arrow.moveDegrees('altitude', testDegrees, config.CW_ALTITUDE, config.SPEED_ALTITUDE_CW);
-      } else if (key.name === "down") {
-        console.log("alt CCW " + testDegrees + " degrees");
-        arrow.moveDegrees('altitude', testDegrees, config.CCW_ALTITUDE, config.SPEED_ALTITUDE_CCW);
-      } else if (key.name === "left") {
-        console.log("azi CCW " + testDegrees + " degrees");
-        arrow.moveDegrees('azimuth', testDegrees, config.CCW_AZIMUTH, config.SPEED_AZIMUTH);
-      } else if (key.name === "right") {
-        console.log("azi CW " + testDegrees + " degrees");
-        arrow.moveDegrees('azimuth', testDegrees, config.CW_AZIMUTH, config.SPEED_AZIMUTH);
-      } else if (key.name === "space") {
-        arrow.moveDegrees('azimuth', 0.1, config.CW_AZIMUTH);
-      }
-      
-  //~ imu.orientation.on("change", function () {
+var testIMU = function() {
+  var imu = new five.IMU({
+    controller: "BNO055",
+    // enableExternalCrystal: false
+  });
+
+  imu.on("data", function() {
     o = arrow.imu.orientation;
     console.log("orientation");
     console.log("  w            : ", o.quarternion.w);
@@ -273,43 +269,6 @@ var testServos = function() {
     console.log("  pitch        : ", o.euler.pitch);
     console.log("--------------------------------------");
 
-  //~ });
-      //~ console.log("Accelerometer");
-      //~ console.log("  x            : ", arrow.imu.accelerometer.x);
-      //~ console.log("  y            : ", arrow.imu.accelerometer.y);
-      //~ console.log("  z            : ", arrow.imu.accelerometer.z);
-      //~ console.log("  pitch        : ", arrow.imu.accelerometer.pitch);
-      //~ console.log("  roll         : ", arrow.imu.accelerometer.roll);
-      //~ console.log("  acceleration : ", arrow.imu.accelerometer.acceleration);
-      //~ console.log("  inclination  : ", arrow.imu.accelerometer.inclination);
-      //~ console.log("  orientation  : ", arrow.imu.accelerometer.orientation);
-      //~ console.log("--------------------------------------");
-      //~ console.log("magnetometer");
-      //~ console.log("  heading : ", Math.floor(arrow.imu.magnetometer.heading));
-      //~ console.log("  bearing : ", arrow.imu.magnetometer.bearing.name);
-      //~ console.log("--------------------------------------");
-      //~ console.log("Gyroscope");
-      //~ console.log("  x            : ", arrow.imu.gyro.x);
-      //~ console.log("  y            : ", arrow.imu.gyro.y);
-      //~ console.log("  z            : ", arrow.imu.gyro.z);
-      //~ console.log("  pitch        : ", arrow.imu.gyro.pitch);
-      //~ console.log("  roll         : ", arrow.imu.gyro.roll);
-      //~ console.log("  yaw          : ", arrow.imu.gyro.yaw);
-      //~ console.log("  rate         : ", arrow.imu.gyro.rate);
-    }
-  });
-};
-
-var testIMU = function() {
-  var arrow = new Arrow();
-  console.log(arrow.imu.gyro.isCalibrated);
-
-  //~ var imu = new five.IMU({
-    //~ controller: "BNO055",
-    //~ // enableExternalCrystal: false
-  //~ });
-
-  //~ imu.on("data", function() {
     //~ console.log("Accelerometer");
     //~ console.log("  x            : ", this.accelerometer.x);
     //~ console.log("  y            : ", this.accelerometer.y);
@@ -317,17 +276,40 @@ var testIMU = function() {
     //~ console.log("  inclination  : ", this.accelerometer.inclination);
     //~ console.log("  pitch        : ", this.accelerometer.pitch);
     //~ console.log("  roll         : ", this.accelerometer.roll);
+    //~ console.log("--------------------------------------");
 
     //~ console.log("magnetometer");
     //~ console.log("  heading : ", Math.floor(this.magnetometer.heading));
-    //~ // console.log("  bearing : ", this.magnetometer.bearing.name);
+    //~ console.log("  bearing : ", this.magnetometer.bearing.name);
     //~ console.log("--------------------------------------");
-  //~ });
+    //~ console.log("Gyroscope");
+    //~ console.log("  x            : ", arrow.imu.gyro.x);
+    //~ console.log("  y            : ", arrow.imu.gyro.y);
+    //~ console.log("  z            : ", arrow.imu.gyro.z);
+    //~ console.log("  pitch        : ", arrow.imu.gyro.pitch);
+    //~ console.log("  roll         : ", arrow.imu.gyro.roll);
+    //~ console.log("  yaw          : ", arrow.imu.gyro.yaw);
+    //~ console.log("  rate         : ", arrow.imu.gyro.rate);
+  });
 };
 
 
+var arrow = null;
 board.on("ready", function() {
-  trackMoon();
+  arrow = new Arrow();
+  arrow.imu.on("data", function() {
+    arrow.azimuth = Math.floor(arrow.imu.orientation.euler.heading);
+    arrow.altitude = -Math.floor(arrow.imu.orientation.euler.pitch);
+    //~ console.log("azimuth: " + arrow.azimuth + " altitude: " + arrow.altitude);
+  });
+
+  manualControl();  // optional testDegrees argument: manualControl(90);
+  console.log("Please calibrate in the next " + (config.INIT_CALIBRATION_INTERVAL / 1000) + " seconds.");
+  //~ setLight();
+  //~ setTimeout(function() {
+    //~ trackMoon();
+  //~ }, config.INIT_CALIBRATION_INTERVAL);
+  
   //~ testIMU();
   //~ testServos();
 });
